f144e11b96d1c0b6f1029e6c3ddb5f90cd5797e1
peihongchen <171250610@smail.nju.edu.cn>
2022-01-29 21:03:43 +0800

    session: deploy session module

--- src/main/java/com/dongdong/backend/config/ZookeeperConfig.java
+++ src/main/java/com/dongdong/backend/config/ZookeeperConfig.java
@@ -1,6 +1,5 @@
 package com.dongdong.backend.config;
 
-import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,13 +24,10 @@ public class ZookeeperConfig {
             final CountDownLatch countDownLatch = new CountDownLatch(1);
             //连接成功后，会回调watcher监听，此连接操作是异步的，执行完new语句后，直接调用后续代码
             //  可指定多台服务地址 127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183
-            zooKeeper = new ZooKeeper(connectString, timeout, new Watcher() {
-                @Override
-                public void process(WatchedEvent event) {
-                    if (Event.KeeperState.SyncConnected == event.getState()) {
-                        //如果收到了服务端的响应事件,连接成功
-                        countDownLatch.countDown();
-                    }
+            zooKeeper = new ZooKeeper(connectString, timeout, event -> {
+                if (Watcher.Event.KeeperState.SyncConnected == event.getState()) {
+                    //如果收到了服务端的响应事件,连接成功
+                    countDownLatch.countDown();
                 }
             });
             countDownLatch.await();
--- src/test/java/com/dongdong/backend/controllers/SessionControllerTests.java
+++ src/test/java/com/dongdong/backend/controllers/SessionControllerTests.java
@@ -8,13 +8,15 @@ import java.net.URISyntaxException;
 class SessionControllerTests {
     @Test
     void sendMessage() throws InterruptedException, URISyntaxException {
-        WebSocketClient client = new WebSocketClient(new URI("ws://localhost:8080/dd/session/dd1"));
+        WebSocketClient client1 = new WebSocketClient(new URI("ws://localhost:8080/dd/session/dd1"));
+        WebSocketClient client2 = new WebSocketClient(new URI("ws://localhost:8080/dd/session/dd2"));
 
         int times = 10;
         while (times-- >= 0) {
             String msg = """
-                    {"sender":"dd1","receiver":"dd2","type":"text","timestamp":"%d","payload":"this is a message"}""";
-            client.sendMessage(String.format(msg, System.currentTimeMillis()));
+                    {"sender":"%s","receiver":"%s","type":"text","timestamp":"%d","payload":"this is a message from %s"}""";
+            client1.sendMessage(String.format(msg, "dd1", "dd2", System.currentTimeMillis(), "dd1"));
+            client2.sendMessage(String.format(msg, "dd2", "dd1", System.currentTimeMillis(), "dd2"));
             Thread.sleep(1000);
         }
     }
