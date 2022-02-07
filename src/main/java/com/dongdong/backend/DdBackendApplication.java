6190c95edebd2ae588846ac7b4471b5acb1380fc
ccccz <1056341513@qq.com>
2022-02-07 14:48:33 +0800

    配置文件[按需更改]

--- src/main/java/com/dongdong/backend/DdBackendApplication.java
+++ src/main/java/com/dongdong/backend/DdBackendApplication.java
@@ -3,11 +3,13 @@ package com.dongdong.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaRepositories
 public class DdBackendApplication {
 
 	public static void main(String[] args) {
